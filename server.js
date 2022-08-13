ee39e4361417bd52f960d65f407264f2b0d1ee1c
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-08-13 11:15:00 +0500

    e

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.use(cors());
 app.use("/v1/api", require("./routers/routersController"));
 
 // PORT
-const PORT = process.env.PORT | 5002;
-app.listen(PORT, () => console.log(`Listening on port ==> ${PORT}...`));
+// const PORT = process.env.PORT | 5002;
+// app.listen(PORT, () => console.log(`Listening on port ==> ${PORT}...`));
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
